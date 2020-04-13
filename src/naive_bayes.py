2cc91eb48ed0dcf539ee5a683ad3b7fd5f205e13
Sourav Verma <sourav.verma@ttu.edu>
2020-04-13 12:22:57 -0500

    updated Naive bayse classifier logic

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -6,18 +6,16 @@ Information Retrieval Assignment 3
 
 """
 import pandas as pd
-import numpy as np
 import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-import matplotlib.patheffects as PathEffects
+from wordcloud import WordCloud
 import seaborn as sns
-from sklearn.manifold import TSNE
-import matplotlib.cm as cm
-import scipy.cluster.hierarchy as shc
+
 
 #read the dataset
 dataset = pd.read_csv("winemag-data.csv")
@@ -81,36 +79,112 @@ for row in tokens_list_ws:
 
 final_dataframe = pd.DataFrame(feature_sets, columns=possible_words)
 final_dataframe['CLASS'] = wine_score_list
+print(final_dataframe)
 
 #CLASS is the label to predict classs of wine so we drop it from dataset
 y = final_dataframe.CLASS
-x = final_dataframe.drop('CLASS', axis=1)
+x = final_dataframe
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
 
 # probability of each class in training dataset Pcj values
-classes = ['Classic','Outstanding','Very Good','Good','Mediocre'] 
+Pcj_dict = {}
+classes = ['Classic','Outstanding','Very Good','Good'] 
 class_dict = dict.fromkeys(classes, 0.0)
 for row in y_train:
     class_dict[row]+=1
 for claass in classes:
     pcj = class_dict[claass]/len(y_train)
-    print('PCj({})={}'.format(claass,pcj))
+    # print('PCj({})={}'.format(claass,pcj))
+    Pcj_dict[claass] = pcj
 
 # Compute p(w|class) using Laplace smoothing method 
+Pwc_dict = {}
+for e_class in classes:
+    fil_df = x_train.loc[lambda df: df['CLASS']== e_class]
+    wk_dict = dict.fromkeys(possible_words, 0.0)
+    n=0 # total number of words in class
+    for i in range(0,len(fil_df)):
+        feature_dict = fil_df.iloc[i,:len(possible_words)+1]
+        for key in possible_words:
+            wk_dict[key]+= feature_dict[key]
+            n+=feature_dict[key]
+    for word in possible_words:
+        nk = wk_dict[word]
+        Pwc = (nk + 1)/ (n+len(possible_words))
+        # print('P({}|{}) = {}'.format(word,e_class,Pwc))
+        wk_dict[word] = Pwc
+    Pwc_dict[e_class] = wk_dict
 
-# for e_class in classes:
-fil_df = final_dataframe.loc[lambda df: df['CLASS']=='Classic']
-wk_dict = dict.fromkeys(possible_words, 0.0)
-n=0 # total number of words in class
-for i in range(0,len(fil_df)):
-    feature_dict = fil_df.iloc[i,:2335]
-    for key in possible_words:
-        wk_dict[key]+= feature_dict[key]
-        n+=feature_dict[key]
-for word in possible_words:
-    nk = wk_dict[word]
-    Pwc = (nk + 1)/ (n+len(possible_words))
-    print('P({}|Classic) = {}'.format(word,Pwc))
+# list of posterior probability
+posterior_prob_list = []
+#calcualte posterior probability for each class
+for idx,val in y_test.iteritems():
+    post_prob_dict = dict.fromkeys(['id','Classic','Outstanding','Very Good','Good'])
+    post_prob_dict['id']=idx
+    token_list = tokens_list_ws[idx]
+    for e_class in classes:
+        post_prob = Pcj_dict[e_class]
+        for token in token_list:
+            post_prob*= Pwc_dict[e_class][token]
+        post_prob_dict[e_class] = post_prob
+    posterior_prob_list.append(post_prob_dict)
+    # print('{} : {}'.format(idx, token_list))
+
+# function to find maximum posterior probability among 4 classes and return the label of the class
+def find_max_post_prob(data):
+    prob=data['Classic']
+    label='Classic'
+    for lbl in ['Outstanding','Very Good','Good']:
+        if data[lbl]>prob:
+            prob = data[lbl]
+            label=lbl
+    return(label,prob)
+# compute the results of classifier
+predicted_results = {}
+for row in posterior_prob_list:
+    claass,max_prob = find_max_post_prob(row)
+    predicted_results.update({ row['id'] : claass })
+
+# Classifier results
+y_predicted = pd.Series(predicted_results, name='CLASS')
+conf_mat = confusion_matrix(y_test, y_predicted, labels=classes)
+print(conf_mat)
+print('\n\n')
+sns.heatmap(conf_mat)
+
+from sklearn.metrics import classification_report 
+
+print(classification_report(y_test, y_predicted))
+
+# word cloud 
+out = []
+gd =[]
+vegd = []
+clasic = []
+for idx,val in y_test.iteritems():
+    token_list = tokens_list_ws[idx]
+    if val=='Classic':
+        clasic+= token_list
+    elif val=='Good':
+        gd+=token_list
+    elif val=='Very Good':
+        vegd+=token_list
+    elif val=='Outstanding':
+        out+=token_list
+
+
+
+wordcloud = WordCloud(width = 800, height = 800, 
+                background_color ='white', 
+                min_font_size = 10).generate(' '.join(clasic))
+
+plt.figure(figsize = (8, 8), facecolor = None) 
+plt.imshow(wordcloud) 
+plt.axis("off") 
+plt.tight_layout(pad = 0) 
+  
+plt.show() 
+
     
     
     
